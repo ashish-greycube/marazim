0afe93ca72d7de55581c8907cea4943e93a45afe
ashish-greycube <mr.ashish.shah@gmail.com>
2024-08-03 12:17:43 +0530

    feat: update stock on si return2

--- marazim/public/js/sales_invoice.js
+++ marazim/public/js/sales_invoice.js
@@ -1,4 +1,4 @@
-frappe.ui.form.on('Sales Invoice', {
+frappe.ui.form.on('Sales Invoic', {
     is_return: function (frm) {
         if (frm.doc.is_return==1 && (frm.doc.return_against==undefined || frm.doc.return_against=='')) {
             frm.set_value('update_stock', 1)
